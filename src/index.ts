81fff65d67aed2319c6e2b8b7a97281d12ea87f2
Un Focused <unfocused@Uns-iMac-3.local>
2023-04-02 02:49:54 -0700

    format big num

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import {
     Connection,
-    GetVersionedTransactionConfig, ParsedInstruction,
-    PartiallyDecodedInstruction,
-    PublicKey, TransactionInstruction,
+    GetVersionedTransactionConfig,
+    PublicKey,
     VersionedTransactionResponse
 } from '@solana/web3.js';
 
@@ -10,7 +9,6 @@ import { SolanaParser } from '@debridge-finance/solana-transaction-parser';
 
 import {
     BorshCoder,
-    BorshInstructionCoder,
     EventData,
     EventParser,
     Idl
@@ -18,13 +16,8 @@ import {
 import idlJSON from './idl.json';
 import { IdlEventField } from '@coral-xyz/anchor/dist/cjs/idl';
 import BN from 'bn.js';
-// import bs58 from 'bs58';
 import { snakeCase } from 'snake-case';
 import { sha256 } from 'js-sha256';
-
-import { Program, Idl as AnchorIDL } from '@project-serum/anchor';
-
-import { main as sandboxMain } from './sandbox';
 
 /**
  * REFERENCES:
@@ -89,6 +82,8 @@ export type TypeEnum = 'bool' | 'f32' | 'f64' | 'i128' | 'i64' | 'publicKey' | '
 function anchorToTypes(type: TypeEnum, data: EventData<IdlEventField, Record<string, never>>, name: string) {
     const value = data[name];
     switch (type) {
+        case 'publicKey':
+            return (value as PublicKey).toBase58();
         // boolean
         case 'bool':
             console.log('casted to boolean');
@@ -96,19 +91,36 @@ function anchorToTypes(type: TypeEnum, data: EventData<IdlEventField, Record<str
         // big number
         case 'u64':
         case 'u128':
-        case 'i64':
+        case 'i64': {
+            const newValue = value as BN;
+            return newValue.toString(10);
+        }
         case 'i128':
             // console.log('casted to BN')
             // console.log('IS THIS A BN', BN.isBN(value));
             // Note: decimals are not supported in this library.
             const newValue = value as BN;
-            return newValue.toString(10);
+            let str = newValue.toString(10, 128);
+            // https://www.anchor-lang.com/docs/javascript-anchor-types
+            // I80F48
+            const parts = str.split('');
+
+            parts.splice(80, 0, '.');
+
+            str = parts.join('');
+            let startIndex = 0;
+            // 000000.123123
+            for (;str[startIndex] === '-' || str[startIndex] === '0' && startIndex < 79; ++startIndex) {}
+            console.log('BEFORE: ', str)
+            console.log('AFTER: ', str.substring(startIndex))
+            return str.substring(startIndex);
+
+            // str = parts.join('').replace(/^0+/, '');
         // number
         case 'u8':
         case 'u16':
         case 'f32':
         case 'f64':
-            console.log('casted to number');
             return value as number;
         default:
             console.warn('UNKNOWN TYPE: ', data[type], type);
@@ -192,7 +204,7 @@ async function main() {
     const eventMap = new Map<string, IDLEvent>();
     for (const event of events) {
         eventMap.set(event.name, event);
-        console.log(event.name);
+        // console.log(event.name);
     }
     const address = '4MangoMjqJ2firMokCjjGgoK8d4MXcrgL7XJaL3w6fVg';
     const pubKey = new PublicKey(address);
@@ -215,24 +227,13 @@ async function main() {
         config
     );
 
-    // const parsedTransactions = await connection.getParsedTransactions(
-    //     signatures.map(
-    //         item => item.signature
-    //     ),
-    //     config
-    // );
-
     const idl: Idl = idlJSON as Idl;
     const coder = new BorshCoder(idl);
     const parser = new EventParser(pubKey, coder);
-    const instructionParser = new BorshInstructionCoder(idl);
 
     const txParser = new SolanaParser([
         { 'idl': idlJSON as any, programId: address }
     ]);
-
-    const layouts = (coder.instruction as any).sighashLayouts;
-    console.log('layouts', layouts);
 
     for (let i = 0; i < transactions.length; ++i) {
         const transaction = transactions[i];
@@ -243,35 +244,53 @@ async function main() {
             continue;
         }
 
-        const parsedArray = txParser.parseTransactionData(transaction.transaction.message, transaction.meta?.loadedAddresses);
-        // console.log('parsed: ', parsedArray);
+        const parsedArray = txParser.parseTransactionData(
+            transaction.transaction.message, transaction.meta?.loadedAddresses);
         const logs = transaction.meta?.logMessages;
         if (!logs) {
             console.log('no logs');
             continue;
         }
         const gen = parser.parseLogs(logs, false);
+        console.log(`TRANSACTION INFO:`);
+        // Apr 2, 2023 at 08:56:26 UTC
+        const date = new Date(blockTime! * 1000);
+        const dateString = date.toLocaleString('en-US', {
+            month: 'short',
+            day: 'numeric',
+            year: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric',
+            second: 'numeric',
+            timeZone: 'UTC',
+            timeZoneName: 'short'
+        });
+        // Apr 2, 2023 at 9:02:54 UTC
+        console.log(`BLOCK TIME: ${ dateString }`);
+        console.log(`SIGNATURE: ${ signature }`);
         for (const next of gen) {
             const { name, data } = next;
+            console.log('DATA', data);
             const event = eventMap.get(name);
             if (!event) {
                 console.log('[INFO]', 'MISSING FOR NAME: ' + name);
                 continue;
             }
             console.log('event found', name);
-            //
-            // for (const field of event.fields) {
-            //     const { name } = field;
-            //     // console.log(`BEFORE CAST: ${ name }: `, data[name]);
-            //     if (typeof field.type !== 'string') {
-            //         console.log('[INFO]: MUST BE A CLASS TYPE');
-            //         continue;
-            //     }
-            //
-            //     const { type } = field;
-            //     const value = anchorToTypes(type, data, name);
-            //     console.log('[INFO] CASTED: ', field.name, value);
-            // }
+            console.log('TX SIGS', transaction.transaction.signatures);
+
+            for (const field of event.fields) {
+                const { name } = field;
+                // console.log(`BEFORE CAST: ${ name }: `, data[name]);
+                if (typeof field.type !== 'string') {
+                    console.log('[INFO]: MUST BE A CLASS TYPE');
+                    continue;
+                }
+
+                const { type } = field;
+                const value = anchorToTypes(type, data, name);
+                console.log('[INFO] CASTED: ', field.name, value);
+            }
         }
         // PerpPlaceOrder
         // want: H4xgvHhm7NU
@@ -282,80 +301,13 @@ async function main() {
         for (const log of logs) {
             console.log('***************');
             if (log.includes('Instruction')) {
-                for (const txInstruction of parsedTransaction.transaction.message.instructions) {
+                for (const parsedInstruction of parsedArray) {
                     // we only want mango program, this assumes all instructions are the same
                     // program in the transaction
-                    if (txInstruction.programId.toBase58() !== address) {
+                    if (parsedInstruction.programId.toBase58() !== address) {
                         continue;
                     }
-                    // console.log(parsedTransaction.transaction.message.instructions);
-                    // program: string, programId: PublicKey, parsed: any full
-                    // programId: PublicKey, accounts: Array<PublicKey>, data: string partial
-                    if ('data' in txInstruction) {
-                        console.log('PARTIALLY DECODED:');
-                        const instruction = txInstruction as PartiallyDecodedInstruction;
-                        console.log(instruction.programId.toBase58());
-                        console.log(instruction.accounts);
-                        console.log(instruction.data);
-                        // const parsed = txParser.parseInstruction(
-                        //     new TransactionInstruction(
-                        //         {
-                        //             data: Buffer.from(instruction.data),
-                        //             programId: instruction.programId,
-                        //             keys: instruction.accounts.map(
-                        //                 (acct) => {
-                        //                     return {
-                        //                         pubkey: acct,
-                        //                         isSigner: false,
-                        //                         isWritable: false
-                        //                     }
-                        //                 }
-                        //             )
-                        //         }
-                        //     )
-                        // )
-                        // const parsed = txParser.parseTransactionParsedData(parsedTransaction.transaction.message)
-                        const parsed = await txParser.parseTransaction(connection, signatures[i].signature)
-                        console.log('PARSED?', parsed);
-                        // console.log(
-                        //     'WHAT IS THIS 1',
-                        //     coder.instruction.decode(instruction.programId.toBase58(), 'base58')
-                        // );
 
-                    } else {
-                        console.log('FULL DECODED:');
-                        const instruction = txInstruction as ParsedInstruction;
-
-                        console.log(instruction.programId);
-                        console.log(instruction.parsed);
-                        console.log(instruction.program);
-                    }
-                    // console.log(versionedMessage.compiledInstructions);
-                    // const startIndex = log.indexOf('Instruction');
-                    // console.log('MAYBE THIS', message.instructions)
-                    // if (versionedMessage.compiledInstructions && versionedMessage.compiledInstructions.length > 0) {
-                    //     const firstInstruction = versionedMessage.compiledInstructions[0];
-                    //     // may not contain any data, no args in IDL, only accounts
-                    //     const { data } = firstInstruction;
-                    //     // console.log('data', data);
-                    //     // const buff = typedArrayToBuffer(firstInstruction.data);
-                    //     const decoded = coder.instruction.decode(data as Buffer, 'base58')
-                    //     // console.log(log)
-                    //     // const decoded = decode(data, coder);
-                    //     console.log('[DECODED]', decoded);
-                    // const ix = s58.decode(data);
-
-                    // console.log('ix', ix);
-                    // console.log('before sighash', ix.slice(0, 8), log);
-                    // const sighash = bs58.encode(ix.slice(0, 8));
-                    // console.log('sighash', sighash)
-                    // const dataPart = ix.slice(8);
-                    // // hack to access the private variable3
-                    // const layouts = (coder.instruction as any).sighashLayouts;
-                    // console.log('layouts', layouts);
-                    // const decoder = layouts.get(sighash);
-                    // console.log('DECODER', decoder);
-                    // console.log(log)
                     const searchStr = 'Instruction: ';
                     const rawInstructionName = log.substring(log.indexOf(searchStr) + searchStr.length);
                     const parts = rawInstructionName.split('');
@@ -366,70 +318,16 @@ async function main() {
                         (instruction) => instruction.name === formattedInstructionName
                     );
 
-                    console.log('formatted name', formattedInstructionName);
+                    // console.log('formatted name', formattedInstructionName);
                     // console.log('formatted name', JSON.stringify({name: formattedInstructionName}));
-                    console.log('found instruction', foundInstruction);
+                    // console.log('found instruction', foundInstruction);
+                    console.log('parsedInstruction:');
+                    console.log('\tname: ', parsedInstruction.name);
+                    // console.log('\targs: ', parsedInstruction.args);
                 }
-                //
-                // if (!foundInstruction) {
-                //     console.log('NOT FOUND!!');
-                //     continue;
-                // }
-                // console.log('INSTRUCTION', foundInstruction);
-
-                // const sh = sighashF('global', foundInstruction.name);
-                // console.log('[INFO] SH', bs58.encode(sh));
-                // sighashLayouts.set(bs58.encode(sh), {
-                //     layout: this.ixLayout.get(ix.name),
-                //     name: ix.name,
-                // });
-                // if (!decoder) {
-                //     return null;
-                // }
-                // return {
-                //     data: decoder.layout.decode(data),
-                //     name: decoder.name,
-                // };
-                // console.log('DATA: ', data)
-                // console.log('DECODED DATA: ', result)
-                // console.log(
-                //     'WHAT IS THIS 1',
-                //     coder.instruction.decode(message.instructions[0].data, 'hex')
-                // );
-                // }
-                // console.log('RAW FORMATTED INSTRUCTION:', log.substring(startIndex));
-                // const rawInstruction = log.substring(startIndex);
-                // console.log('WHAT IS THIS', coder.instruction.decode(rawInstruction));
             }
         }
-        // const parsed = instructionParser.decode(message.serialize());
-        // if (message.instructions && message.instructions.length > 0) {
-        //     const parsed = instructionParser.decode(message.instructions[0].data, 'base58');
-        //     // const parsed = coder.instruction.decode(ogs.join(' '), 'base58');
-        //     // console.log('INSTRUCTIONS', message.instructions[0].data);
-        //     console.log('[INSTRUCTIONS]', parsed);
-        //     console.log(logs)
-        //     // if (parsed != null) {
-        //     // }
-        // }
-        // console.log('ACCOUNT KEYS', message.accountKeys);
-        // console.log('INSTRUCTIONS', message.instructions);
     }
-
-    // connection.onLogs(
-    //     pubKey,
-    //     (logs, ctx) => {
-    //         const { logs: innerLogs } = logs;
-    //         const instructions = innerLogs.filter(
-    //             (log) => log.includes('Instruction')
-    //         );
-    //
-    //         console.log('[INFO]', instructions);
-    //     }
-    // );
-
-// connection.getTransactions()
 }
 
 void main();
-// void sandboxMain();
