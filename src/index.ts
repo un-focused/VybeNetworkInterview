cdb792debb037819ba5076fe9f093a796aba3db7
Un Focused <unfocused@Uns-iMac-3.local>
2023-04-02 20:41:36 -0700

    idl parsing

--- src/index.ts
+++ src/index.ts
@@ -13,10 +13,11 @@ import {
     EventParser,
     Idl
 } from '@coral-xyz/anchor';
-import idlJSON from './idl.json';
-import { IdlEventField } from '@coral-xyz/anchor/dist/cjs/idl';
+// import idlJSON from './idl.json';
+import { IdlEvent, IdlEventField, IdlType, IdlTypeDefined } from '@coral-xyz/anchor/dist/cjs/idl';
 import BN from 'bn.js';
 import {
+    DEV_NET_SOLANA_RPC_ENDPOINT,
     MAIN_NET_SOLANA_RPC_ENDPOINT, MAIN_NET_SOLANA_RPC_POLL_MS,
     MAIN_NET_SOLANA_RPC_RATE_LIMIT,
     MANGO_V4_IDL,
@@ -58,29 +59,31 @@ function uppercaseWords(words: string[]) {
     return words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
 }
 
-export interface IDLEvent {
-    name: string;
-    fields: Field[];
-}
-
-export interface Field {
-    name: string;
-    type: TypeClass | TypePrimitive;
-    index: boolean;
-}
-
-export interface TypeClass {
-    defined?: string;
-    vec?: Vec;
-}
-
-export interface Vec {
-    defined: string;
-}
+// export interface IDLEvent {
+//     name: string;
+//     fields: Field[];
+// }
+//
+// export interface Field {
+//     name: string;
+//     type: TypeClass | TypePrimitive;
+//     index: boolean;
+// }
+//
+// export interface TypeClass {
+//     defined?: string;
+//     vec?: Vec;
+// }
+//
+// export interface Vec {
+//     defined: string;
+// }
 
 // u32, i8, & i16 are omitted as it is not in the events type
-export type TypePrimitive = 'bool' | 'f32' | 'f64' | 'i128' | 'i64' | 'publicKey' | 'u128' | 'u16' | 'u64' | 'u8';
+// 'bool' | 'f32' | 'f64' | 'i128' | 'i64' | 'publicKey' | 'u128' | 'u16' | 'u64' | 'u8';
+export type TypePrimitive = "bool" | "u8" | "i8" | "u16" | "i16" | "u32" | "i32" | "f32" | "u64" | "i64" | "f64" | "u128" | "i128" | "u256" | "i256" | "bytes" | "string" | "publicKey";
 
+// TODO: parse primitives
 function getFormattedField(type: TypePrimitive, data: EventData<IdlEventField, Record<string, never>>, name: string) {
     const value = data[name];
     switch (type) {
@@ -93,10 +96,10 @@ function getFormattedField(type: TypePrimitive, data: EventData<IdlEventField, R
         case 'u64': {
             // return I80F48.fromU64(value as BN).toString();
             const newValue = value as BN;
-            const n = BigInt(newValue.toString(10))
+            const n = BigInt(newValue.toString(10));
             const formatter = new Intl.NumberFormat('en-US');
 
-            return formatter.format(n)
+            return formatter.format(n);
         }
         case 'u128':
         case 'i64': {
@@ -110,7 +113,7 @@ function getFormattedField(type: TypePrimitive, data: EventData<IdlEventField, R
         }
         case 'i128': {
             const newValue = value as BN;
-            const n = BigInt(newValue.toString(10))
+            const n = BigInt(newValue.toString(10));
             const formatter = new Intl.NumberFormat('en-US');
 
             return formatter.format(n);
@@ -142,12 +145,96 @@ function getTransactionsForSignatures(connection: Connection, signatures: Confir
     );
 }
 
-async function mainBody(connection: Connection, eventMap: Map<string, IDLEvent>) {
-    const signatures = await connection.getSignaturesForAddress(MANGO_V4_PUBLIC_KEY,
-        {
-            limit: MAIN_NET_SOLANA_RPC_RATE_LIMIT
+function parseEvent(data: EventData<IdlEventField, Record<string, never>>, fields: IdlEventField[]) {
+    for (const { name, type } of fields) {
+        const parts = splitCamelCaseString(name);
+        const displayName = uppercaseWords(parts).join(' ');
+        // u64" | "i64" | "f64" | "u128" | "i128" | "u256" | "i256" | "bytes" | "string" | "publicKey" |
+        //  | IdlTypeOption | IdlTypeCOption | IdlTypeVec | IdlTypeArray;
+        if (typeof (type) === 'string') {
+            const primitiveType = type as TypePrimitive;
+            // must be a type enum
+            console.log(`[${ displayName }]`, getFormattedField(primitiveType, data, name));
+            // IdlTypeDefined
+        } else if ('defined' in type) {
+            console.log('[TYPE DEFINED]', type.defined);
+            const { defined } = type;
+            if (!MANGO_V4_IDL.types) {
+                continue;
+            }
+
+            const idlTypeDef = MANGO_V4_IDL.types.find(
+                (type) => type.name === defined
+            );
+
+            if (!idlTypeDef) {
+                console.log('WHAT IS NOT FOUND: ', defined, type);
+                continue;
+            }
+
+            console.log('name', name);
+            console.log('IDLTypeDef', idlTypeDef);
+            console.log('[DATA INSIDE]', data[name]);
+            const innerData = data[name] as EventData<IdlEventField, Record<string, never>>;
+            const { type: innerType } = idlTypeDef;
+            if (innerType.kind === 'enum') {
+                if (!innerType.variants) {
+                    throw 'enum must have variants';
+                }
+
+                for (const key of Object.keys(innerData as any)) {
+                    for (const variant of innerType.variants) {
+                        if (variant.name.toLowerCase() === key.toLowerCase()) {
+                            console.log('NAME: ' + key + ' FOUND');
+                            break;
+                        }
+                    }
+                }
+            } else if (innerType.kind === 'struct') {
+                const eventFields = innerType.fields.map(
+                    ({ type, name }): IdlEventField => {
+                        return {
+                            type,
+                            name,
+                            index: false
+                        }
+                    }
+                )
+                // IdlEventField
+                parseEvent(innerData, eventFields);
+            }
+        } else if ('option' in type) { // IdlTypeOption
+            // in our IDL, the only option is u32
+            const option = type.option;
+            if (typeof option !== 'string') {
+                throw 'IDL should not have options without strings: ' + type
+            }
+
+            // recursive
+            console.log(`[${ displayName }]`, getFormattedField(option as TypePrimitive, data, name));
+        } else if ('coption' in type) { // IdlTypeCOption
+        } else if ('vec' in type) { // IdlTypeVec
+        } else if ('array' in type) { // IdlTypeArray
         }
-    );
+    }
+}
+
+async function mainBody(connection: Connection, eventMap: Map<string, IdlEvent>) {
+    // const signatures = await connection.getSignaturesForAddress(MANGO_V4_PUBLIC_KEY,
+    //     {
+    //         limit: MAIN_NET_SOLANA_RPC_RATE_LIMIT
+    //     }
+    // );
+    const signatures: ConfirmedSignatureInfo[] = [
+        {
+            confirmationStatus: 'finalized',
+            blockTime: new Date().getTime() / 1000,
+            slot: 1,
+            err: null,
+            memo: null,
+            signature: '4WEPK9aRByW4NvDJDpL8Uk716URjdPgD2mT8oXDGswmnZkzPx1UU2McyVeJzdrq7ofR8Nth7rY7hzyTCrJiBiH73'
+        }
+    ];
 
     const transactions = await getTransactionsForSignatures(connection, signatures);
     const coder = new BorshCoder(MANGO_V4_IDL);
@@ -156,7 +243,7 @@ async function mainBody(connection: Connection, eventMap: Map<string, IDLEvent>)
     for (let i = 0; i < transactions.length; ++i) {
         const transaction = transactions[i];
         // TODO: convert to date time
-        const { blockTime, signature } = signatures[i];
+        const { blockTime, signature, confirmationStatus } = signatures[i];
         if (!transaction) {
             console.log('[INFO], NULL');
             continue;
@@ -169,7 +256,7 @@ async function mainBody(connection: Connection, eventMap: Map<string, IDLEvent>)
         }
 
         const gen = parser.parseLogs(logs, false);
-        console.log(`TRANSACTION INFO:`);
+        // console.log(`TRANSACTION INFO:`);
         // Apr 2, 2023 at 08:56:26 UTC
         // TODO: check status before using block time
         const date = new Date(blockTime! * 1000);
@@ -186,6 +273,7 @@ async function mainBody(connection: Connection, eventMap: Map<string, IDLEvent>)
         // Apr 2, 2023 at 9:02:54 UTC
         console.log(`BLOCK TIME: ${ dateString }`);
         console.log(`SIGNATURE: ${ signature }`);
+        console.log(`CONFIRMATION STATUS: ${ confirmationStatus }`);
         // console.log('LOADED: ', transaction.meta?.loadedAddresses);
         for (const next of gen) {
             const { name, data } = next;
@@ -194,20 +282,22 @@ async function mainBody(connection: Connection, eventMap: Map<string, IDLEvent>)
                 console.log('[INFO]', 'MISSING FOR NAME: ' + name);
                 continue;
             }
-            for (const { name, type } of event.fields) {
-                const parts = splitCamelCaseString(name);
-                const displayName = uppercaseWords(parts).join(' ');
-                if (typeof(type) === 'string') {
-                    const primitiveType = type as TypePrimitive;
-                    // must be a type enum
-                    console.log(`[${ displayName }]`, getFormattedField(primitiveType, data, name));
-                } else {
-                    // must be a type class
-                    console.log('CLASS TIME DING DING DING: ', type);
-                    console.log('DATA: ', data);
-                }
+
+            if (event.name === 'MangoAccountData' || event.name === 'FlashLoanLog' || event.name === 'WithdrawLoanOriginationFeeLog') {
+                console.log('SUPER BIG IMPORTANT LOG HERE!!!');
+                console.log(`BLOCK TIME: ${ dateString }`);
+                console.log(`SIGNATURE: ${ signature }`);
+                console.log(`CONFIRMATION STATUS: ${ confirmationStatus }`);
+                console.log('DATA', data);
             }
+            // } else {
+            //     // console.log('DOES NOT MATTER FOR NOW: ', name);
+            //     continue;
+            // }
+
+            // TODO: parse event found
             console.log('event found', name);
+            parseEvent(data, event.fields);
             // console.log('TX SIGS', transaction.transaction.signatures);
 
             // for (const field of event.fields) {
@@ -226,18 +316,7 @@ async function mainBody(connection: Connection, eventMap: Map<string, IDLEvent>)
     }
 }
 
-function sleep(timeMS: number) {
-    return new Promise<void>(
-        (resolve) => {
-            setTimeout(
-                () => resolve(),
-                timeMS
-            )
-        }
-    );
-}
-
-async function runner(connection: Connection, eventMap: Map<string, IDLEvent>) {
+async function runner(connection: Connection, eventMap: Map<string, IdlEvent>) {
     console.log('EXECUTING RUNNER');
     await mainBody(connection, eventMap);
     console.log('FINISHED RUNNER');
@@ -256,8 +335,9 @@ async function runner(connection: Connection, eventMap: Map<string, IDLEvent>) {
 //     }
 // ];
 async function main() {
-    const events = idlJSON.events as IDLEvent[];
-    const eventMap = new Map<string, IDLEvent>();
+    // we know events must be defined
+    const events = MANGO_V4_IDL.events!;
+    const eventMap = new Map<string, IdlEvent>();
     for (const event of events) {
         eventMap.set(event.name, event);
     }
@@ -265,9 +345,11 @@ async function main() {
     // Write some code to establish a connection to Solana mainnet via an RPC endpoint, you can use this for
     // free: https://docs.solana.com/cluster/rpc-endpoints#mainnet-beta
     const connection = new Connection(MAIN_NET_SOLANA_RPC_ENDPOINT);
+    // const connection = new Connection(DEV_NET_SOLANA_RPC_ENDPOINT);
 
     console.log(`connecting to ${ connection.rpcEndpoint }`);
-    await runner(connection, eventMap);
+    // await runner(connection, eventMap);
+    await mainBody(connection, eventMap);
 }
 
 void main();
